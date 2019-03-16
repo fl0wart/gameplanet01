29576e0f08b27c61aed7ff0520486d0f36d9ee7d
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-16 09:42:30 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,7 +2,6 @@ import asyncio
 import discord
 from discord.ext.commands import Bot
 
-Bot.loop.create_task(status_task())
 
 Mike = Bot('gp!')
 
@@ -11,9 +10,6 @@ async def say(ctx, *args):
     mesg = ' '.join(args)
     await Mike.delete_message(ctx.message)
     return await Mike.say(mesg)
-@bot.event	                                                
-async def status_task():
-    while True:
-        await bot.change_presence(game=discord.Game(name='GamePlanet 👀', type=3))
-        
+
+await bot.change_presence(game=discord.Game(name='GamePlanet! 👀', type=3))
 Mike.run(NTU2Mzc1NDg1NjM5MTYzOTI1.D241eA.mX9gcWly0SQn9yLIEzSCtwwmdRQ)
