795eb0ab238480434e9613017b9997fd0dd14de7
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-19 10:54:00 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -11,4 +11,7 @@ async def say(ctx, *args):
     await Mike.delete_message(ctx.message)
     return await Mike.say(mesg)
 
+game = discord.Game("with the users!")
+await client.change_presence(status=discord.Status.dnd, activity=game)
+
 Mike.run('NTU2Mzc1NDg1NjM5MTYzOTI1.D241eA.mX9gcWly0SQn9yLIEzSCtwwmdRQ')
