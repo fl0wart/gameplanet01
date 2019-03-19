6bd3f83d16f513b9dce959f8fee497043935325d
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-19 11:04:20 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,12 +5,18 @@ from discord.ext.commands import Bot
 
 Mike = Bot('gp!')
 
+@Mike.event
+async def on_ready():
+    print ("Starting up")
+    print ("My username is " + bot.user.name + " and i am running with the ID: " + bot.user.id)
+    await bot.change_presence(game=discord.Game(name="the users", type=3))
+    print ("Started")
+    
 @Mike.command(pass_context = True)
 async def say(ctx, *args):
     mesg = ' '.join(args)
     await Mike.delete_message(ctx.message)
     return await Mike.say(mesg)
 
-game = discord.Game("with the users!")
 
 Mike.run('NTU2Mzc1NDg1NjM5MTYzOTI1.D241eA.mX9gcWly0SQn9yLIEzSCtwwmdRQ')
